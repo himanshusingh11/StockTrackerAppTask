acd5a04ab8d8e744c741659f2d9b4fa03a4c8f1d
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-11-12 02:42:18 +0530

    fixed time interval

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -14,7 +14,7 @@ async function getStocks(req, res) {
 const updateStockPrices = () => {
   setInterval(async () => {
     try {
-      const stocks = await Stock.find({}).maxTimeMS(0).exec();
+      const stocks = await Stock.find({}).maxTimeMS(10000).exec();
       stocks.forEach(async (stock) => {
         const newPrice = Math.random() * 1000;
         await Stock.updateOne({ _id: stock._id }, { $set: { price: newPrice } });
@@ -22,10 +22,10 @@ const updateStockPrices = () => {
     } catch (error) {
       console.error('Error updating stock prices:', error);
     }
-  }, 500);
+  }, 5000);
 };
 
-setTimeout(updateStockPrices, 500);
+setTimeout(updateStockPrices, 10000);
 
 module.exports = {
   getStocks,
