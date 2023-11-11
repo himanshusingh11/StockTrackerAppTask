4313415e7c13b6c205d2c53eef996eaa97e4694c
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-11-12 03:06:21 +0530

    added code in backend

--- backend/controllers/stockController.js
+++ backend/controllers/stockController.js
@@ -11,21 +11,21 @@ async function getStocks(req, res) {
   }
 }
 
-// const updateStockPrices = () => {
-//   setInterval(async () => {
-//     try {
-//       const stocks = await Stock.find({}).maxTimeMS(10000).exec();
-//       stocks.forEach(async (stock) => {
-//         const newPrice = Math.random() * 1000;
-//         await Stock.updateOne({ _id: stock._id }, { $set: { price: newPrice } });
-//       });
-//     } catch (error) {
-//       console.error('Error updating stock prices:', error);
-//     }
-//   }, 5000);
-// };
+const updateStockPrices = () => {
+  setInterval(async () => {
+    try {
+      const stocks = await Stock.find({}).maxTimeMS(10000).exec();
+      stocks.forEach(async (stock) => {
+        const newPrice = Math.random() * 1000;
+        await Stock.updateOne({ _id: stock._id }, { $set: { price: newPrice } });
+      });
+    } catch (error) {
+      console.error('Error updating stock prices:', error);
+    }
+  }, 5000);
+};
 
-// setTimeout(updateStockPrices, 10000);
+setTimeout(updateStockPrices, 10000);
 
 module.exports = {
   getStocks,
