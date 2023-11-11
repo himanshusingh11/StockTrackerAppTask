af958f226179c7a834fdb43e00bf01380acc7a1b
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-11-12 04:41:11 +0530

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -23,8 +23,8 @@ app.use((req, res, next) => {
 // app.use(cors());
 app.use(
   cors({
-    origin: ["https://stock-tracker-app-task.vercel.app/"],
-    methods:["POST","GET"],
+    origin: "*",
+    methods:["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials:true
   })
 );
