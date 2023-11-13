2e8d9d57969c87613864e9775c02eb3bfab5e425
Himanshu <himanshu.singh_iot18@gla.ac.in>
2023-11-13 12:37:25 +0530

    changed url

--- frontend/stock-tracker-frontend/src/App.js
+++ frontend/stock-tracker-frontend/src/App.js
@@ -16,8 +16,8 @@ function App() {
   useEffect(() => {
     const fetchStockOptions = async () => {
       try {
-        // const response = await axios.get('https://stock-tracker-backend-nu.vercel.app/api/stocks');4
-        const response = await axios.get('https://backendservice-ueok.onrender.com/api/stocks');
+        const response = await axios.get('https://stock-tracker-backend-nu.vercel.app/api/stocks');
+        // const response = await axios.get('https://backendservice-ueok.onrender.com/api/stocks');
         // const response = await axios.get('https://backendservice-ueok.onrender.com/api/stocks', {
         //   withCredentials: true,
         //   headers: {
